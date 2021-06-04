d3c567dcef6996e3608b12b80dc75b1baca884ec
Adrien Rousselet <adrien.rousselet@thetribe.io>
2021-06-04 12:04:31 +0200

    refacto Alarm's psiValue management

--- Javascript/tire-pressure-monitoring-system-tests/alarm-test.js
+++ Javascript/tire-pressure-monitoring-system-tests/alarm-test.js
@@ -4,32 +4,42 @@ chai.should();
 
 var Alarm = require('../tire-pressure-monitoring-system/alarm.js');
 
+/*
+Pour gagner du temps, j'ai décider de dupliquer les Sensor. 
+C'est moche et j'imagine qu'iil me faudrait sans doutes une factory
+ou un truc dans le genre mais je préfère me refocus sur l'exo de
+refacto de Alarm (même si finalement ça en fait partie)
+*/
+
 describe('Tyre Pressure Monitoring System', function() {
 
 	describe('Alarm', function() {
 
 		it('should have an alarm initialized to false', function() {
-			var alarm = new Alarm();
+			const DefaultSensor = require('./sensor-low-pressure.js');
+			var alarm = new Alarm(DefaultSensor);
 			alarm.alarmOn().should.equal(false);
 		});
-
+		
 		it('should set the alarmOn to true if the pressure is lower than 17', function() {
-			var alarm = new Alarm();
-			alarm.check(16)
+			const SensorForLowPressure = require('../tire-pressure-monitoring-system-tests/sensor-low-pressure.js');
+			var alarm = new Alarm(SensorForLowPressure);
+			alarm.check() // pressure 16
 			alarm.alarmOn().should.equal(true)
-			alarm.check(17)
+			alarm.check() // pressure 17
 			alarm.alarmOn().should.equal(false)
-			alarm.check(15)
+			alarm.check() // pressure 15
 			alarm.alarmOn().should.equal(true)
 		})
-
+		
 		it('should set the alarmOn to true if the pressure is greather than 21', function() {
-			var alarm = new Alarm();
-			alarm.check(21)
+			const SensorForHighPressure = require('../tire-pressure-monitoring-system-tests/sensor-high-pressure.js');
+			var alarm = new Alarm(SensorForHighPressure);
+			alarm.check() // pressure 21
 			alarm.alarmOn().should.equal(false)
-			alarm.check(22)
+			alarm.check() // pressure 22
 			alarm.alarmOn().should.equal(true)
-			alarm.check(20)
+			alarm.check() // pressure 20
 			alarm.alarmOn().should.equal(false)
 		})
 	});
--- Javascript/tire-pressure-monitoring-system/alarm.js
+++ Javascript/tire-pressure-monitoring-system/alarm.js
@@ -1,6 +1,11 @@
-var Sensor = require('./sensor.js');
+/* J'ai sortis la dépendance à Sensor dans l'idée
+de pouvoir utiliser une version "mocké" dans les tests.
+Dans l'idéal, je ferai tout le refacto pour entre dans 
+de l'archi hexa (et avoir un inMemorySensor) mais ce
+n'est pas le but de l'exo.
+*/
 
-Alarm = function() {
+Alarm = function(Sensor) {
 	this._lowPressureThreshold = 17;
 	this._highPressureThreshold = 21;
 	this._sensor = new Sensor();
@@ -9,8 +14,14 @@ Alarm = function() {
 
 Alarm.prototype = {
 
-	check: function(psiPressureValue) {
+	check: function() {
+		const psiPressureValue = this.getPsiPressure();
+
 		this._alarmOn = this.shouldTriggerAlarm(psiPressureValue);
+	},
+
+	getPsiPressure: function() {
+		return this._sensor.popNextPressurePsiValue();
 	},
 
 	alarmOn: function() {
